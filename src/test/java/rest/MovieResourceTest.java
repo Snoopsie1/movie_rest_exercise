b2000458b3581328e2b217036b1a880ec452face
Snoopsie1 <oliver.rasoli@gmail.com>
2022-02-24 10:59:03 +0100

    fixed error in rest-tests

--- src/test/java/rest/MovieResourceTest.java
+++ src/test/java/rest/MovieResourceTest.java
@@ -80,7 +80,7 @@ public class MovieResourceTest
     @Test
     public void testServerIsUp() {
         System.out.println("Testing is server UP");
-        given().when().get("/xxx").then().statusCode(200);
+        given().when().get("/movie").then().statusCode(200);
     }
 
     //This test assumes the database contains two rows
@@ -88,7 +88,7 @@ public class MovieResourceTest
     public void testDummyMsg() throws Exception {
         given()
                 .contentType("application/json")
-                .get("/xxx/").then()
+                .get("/movie/").then()
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
                 .body("msg", equalTo("Hello World"));
