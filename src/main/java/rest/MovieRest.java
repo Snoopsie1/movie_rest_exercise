94c01bbfe5420d73f62c1487e959f73f758fcd96
Snoopsie1 <oliver.rasoli@gmail.com>
2022-02-24 10:40:20 +0100

    made gson-json for count

--- src/main/java/rest/MovieRest.java
+++ src/main/java/rest/MovieRest.java
@@ -9,6 +9,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 //Todo Remove or change relevant parts before ACTUAL use
 @Path("xxx")
@@ -25,12 +26,14 @@ public class MovieRest
     public String demo() {
         return "{\"msg\":\"Hello World\"}";
     }
+
     @Path("count")
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public String getRenameMeCount() {
+    public Response getRenameMeCount() {
         long count = FACADE.getMovieCount();
         //System.out.println("--------------->"+count);
-        return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
+        return Response.ok().entity(GSON.toJson(count)).build();  //Done manually so no need for a DTO
     }
+
 }
